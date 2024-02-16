27b466b7cd82aee04aaa15d298a9c0ec4d842012
Matthew Carbone <x94carbone@gmail.com>
2024-02-16 12:00:22 -0500

    Continue to debug action

--- action.sh
+++ action.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 echo action path is "$ACTION_PATH"
-echo init path is "$INIT_PATH"
+echo package path is "$PACKAGE_PATH"
 echo verison is "$MY_VERSION"
 echo github workspace is "$GITHUB_WORKSPACE"
+
+version_path="$PACKAGE_PATH/__version__.py"
+
+if [ -f "$version_path" ]; then
+    # overwrites the existing __version__ by appending to the last line
+    echo "__version__ = $MY_VERSION" >> "$version_path"
+else
+    echo "__version__ = $MY_VERSION" > "$version_path"
+fi
+
+echo __version__ looks like:
+cat "$version_path"
